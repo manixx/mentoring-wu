c17cf890a856aaefcf5c64f02f07e59dec1687fb
Manfred Mjka <manfred.mjka@sclable.com>
2019-10-03 16:05:05 +0200

    initial commit

---
+++ src/environments/firebase.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBDymb766rv1Zaf54UdlEMgRnFKFVfD0dY",
+  authDomain: "mentoring-wu.firebaseapp.com",
+  databaseURL: "https://mentoring-wu.firebaseio.com",
+  projectId: "mentoring-wu",
+  storageBucket: "mentoring-wu.appspot.com",
+  messagingSenderId: "968985758961",
+  appId: "1:968985758961:web:d8a712cab47310c6d7ad50"
+}
